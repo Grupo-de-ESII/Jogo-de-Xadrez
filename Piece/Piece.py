9397e45846610d0d99293bae013186eb425d29f7
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2018-08-21 11:16:25 -0300

    Initial setup
    please, IA team, create the abstract methods that you need to the IA
    and I(João Pedro Abreu de Souza) will implement in all pieces
    
    time elapsed : 2:17 h

---
+++ Piece/Piece.py
@@ -0,0 +1,23 @@
+import Pawn, Rook, Knight, Bishop, Queen,King
+from abc import ABC,abstractmethod
+
+class Piece(ABC):
+	def __init__(self, name,position,player):
+		l={
+			'pawn' : Pawn,
+			'rook' : Rook,
+			'knight' : Knight,
+			'bishop' : Bishop,
+			'queen' : Queen,
+			'king' : King}
+		return l[name](position,player)
+	
+	@abstractmethod
+	def getPossibleMoves(self,board):
+		pass
+
+	@abstractmethod
+	def move(self,position):
+		pass
+	
+
