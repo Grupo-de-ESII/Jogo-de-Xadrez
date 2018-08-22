28891e5627a713e08c3b0ba5f32d410553365579
rbleardini <raphaelleardini@hotmail.com>
2018-08-22 12:25:12 -0300

    Apenas teste de git (8 min)

--- Interface/IU.py
+++ Interface/IU.py
@@ -2,11 +2,12 @@ import pygame
 
 def interface():
     pygame.init()
-    tela = pygame.display.set_mode((300,300))
-    pygame.display.set_caption('Xadrez')
-    myfont = pygame.font.SysFont('Comic Sans MS', 30)
-    textsurface = myfont.render('Omelette au fromage', False, (255,255,255))
-    tela.blit(textsurface, (0,0))
+    info = pygame.display.Info() #cria objeto do Info com atributos da tela
+    tela = pygame.display.set_mode((info.current_w,info.current_h)) #define tamanho da tela
+    pygame.display.set_caption('Xadrez') #define título para tela
+    minhafonte = pygame.font.SysFont('Comic Sans MS', 30)
+    textsurface = minhafonte.render('Omelette au fromage', False, (255,255,255))
+    tela.blit(textsurface, (0, 0))
     pygame.display.update()
     running = True
     while running:
