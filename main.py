e7cc10eb15d5c360eb1d63aa6aad0254842d9c13
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2018-08-24 08:55:27 -0300

    atualização da main para usar tabuleiro
    tentativa de conserto dos testes da maquina de regras

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import Board,Player
+import Tabuleiro,Jogador
 
 
 
--- testes/maquinaRegras.py
+++ testes/maquinaRegras.py
@@ -1,6 +1,6 @@
 import pytest
 
-import Jogo-de-Xadrez.Tabuleiro
+from ..Tabuleiro import *
 
 def test_configuracao_inicial():
 	t=Tabuleiro()
