fa833c8db27c68ddc75d666b9c84e492dc8199d4
Wallace Baleroni <wallacebaleroni@gmail.com>
2018-10-15 15:57:06 -0300

    melhorias no esboço da IA (1h)

--- IA/IA.py
+++ IA/IA.py
@@ -1,117 +1,55 @@
 import sys
+import random
 
+def get_melhor_jogada(tabuleiro, jogador1):
+	jogadas = get_jogadas(tabuleiro, jogador1)
 
-def get_jogadas():
-    return [[[1, 0], [0, 1]], [[-1, 0], [0, -1]]]
+	melhor_valor = -sys.maxsize - 1  # Menor inteiro
+	melhor_jogada = None
 
+	for jogada in jogadas:
+		valor_jogada = minimax(tabuleiro, 3, jogador1)
 
-def joga_jogada(jogada):
-    return [[]]
+		if valor_jogada > melhor_jogada:
+			melhor_valor = valor_jogada
+			melhor_jogada = jogada
 
+	return melhor_jogada
+
+def get_jogadas(tabuleiro, jogador1):
+	return [[[1, 0], [-1, 0]],
+			[[0, 1], [0, -1]]]
+
+def minimax(tabuleiro, profundidade, maximizando_player1):
+	# Limite da profundidade atingido
+	if profundidade == 0:  # TODO: Ou jogada é final
+		return avalia_tabuleiro(tabuleiro, maximizando_player1)
+
+	jogadas = get_jogadas()
+	if maximizando_player1:
+		melhor_valor = -sys.maxsize - 1  # Menor inteiro
+		
+		for jogada in jogadas:
+			tabuleiro = joga_jogada(tabuleiro, jogada)
+			melhor_jogada = max(melhor_jogada, minimax(tabuleiro, profundidade - 1, !maximizando_player1))
+	else:
+		melhor_valor = sys.maxsize  # Maior inteiro
+
+		for jogada in jogadas:
+			tabuleiro = joga_jogada(tabuleiro, jogada)
+			melhor_jogada = min(melhor_jogada, minimax(tabuleiro, profundidade - 1, !maximizando_player1))
+
+	return melhor_jogada
 
 def avalia_tabuleiro(tabuleiro, maximizando_player1):
-    return 1
+	valor_heuristica = heuristica(tabuleiro)
+	if maximizando_player1:
+		return valor_heuristica
+	else:
+		return -valor_heuristica
 
+def heuristica(tabuleiro):
+	return random.random() * 10
 
-def min_max_root(tabuleiro, profundidade, maximizando_player1):
-    # Limite da profundidade atingido
-    if profundidade == 0:  # TODO: Ou jogada é final
-        return avalia_tabuleiro(tabuleiro, maximizando_player1)
-
-    if maximizando_player1:
-        max_h = - sys.maxsize - 1  # Menor inteiro
-        max_jogada = None
-
-        fronteira = get_jogadas()
-        explorado = []
-
-        for jogada in fronteira:
-            fronteira.remove(jogada)
-
-            if jogada not in explorado:  # Para cada jogada
-                explorado.append(jogada)
-
-                tabuleiro = joga_jogada(jogada)  # Vê resultado dela
-
-                # Vê resultado do resto da árvore
-                resultado = min_max(tabuleiro, profundidade - 1, not maximizando_player1)
-
-                # Escolhe o melhor
-                if resultado > max_h:
-                    max_h = resultado
-                    max_jogada = jogada
-
-        return max_jogada
-    else:
-        min_h = sys.maxsize  # Maior inteiro
-        min_jogada = None
-
-        fronteira = get_jogadas()
-        explorado = []
-
-        for jogada in fronteira:
-            fronteira.remove(jogada)
-
-            if jogada not in explorado:  # Para cada jogada
-                explorado.append(jogada)
-
-                tabuleiro = joga_jogada(jogada)  # Vê resultado dela
-
-                # Vê resultado do resto da árvore
-                resultado = min_max(tabuleiro, profundidade - 1, not maximizando_player1)
-
-                # Escolhe o melhor
-                if resultado > min_h:
-                    min_h = resultado
-                    min_jogada = jogada
-
-        return min_jogada
-
-
-def min_max(tabuleiro, profundidade, maximizando_player1):
-    # Limite da profundidade atingido
-    if profundidade == 0:  # Ou jogada é final
-        return avalia_tabuleiro(tabuleiro, maximizando_player1)
-
-    if maximizando_player1:
-        max_h = - sys.maxsize - 1  # Menor inteiro
-
-        fronteira = get_jogadas()
-        explorado = []
-
-        for jogada in fronteira:  # Para cada jogada
-            fronteira.remove(jogada)
-
-            if jogada not in explorado:
-                explorado.append(jogada)
-
-                tabuleiro = joga_jogada(jogada)  # Vê resultado dela
-
-                # Vê resultado do resto da árvore
-                resultado = min_max(tabuleiro, profundidade - 1, not maximizando_player1)
-
-                # Escolhe o melhor
-                if resultado > max_h:
-                    max_h = resultado
-
-        return max_h
-    else:
-        min_h = sys.maxsize  # Maior inteiro
-
-        fronteira = get_jogadas()
-        explorado = []
-
-        for jogada in fronteira:  # Para cada jogada
-            fronteira.remove(jogada)
-
-            if jogada not in explorado:
-
-                tabuleiro = joga_jogada(jogada)  # Vê resultado dela
-
-                # Vê resultado do resto da árvore
-                resultado = min_max(tabuleiro, profundidade - 1, not maximizando_player1)
-
-                if resultado > min_h:
-                    min_h = resultado
-
-        return min_h
+def joga_jogada(tabuleiro, jogada):
+	return tabuleiro
