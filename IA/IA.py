175bca13d55117e54f9f3f58218f8df669f1f70e
Wallace Baleroni <wallacebaleroni@gmail.com>
2018-10-15 19:34:56 -0300

    Heuristica simples pronta (2h)

--- IA/IA.py
+++ IA/IA.py
@@ -1,55 +1,87 @@
 import sys
 import random
 
-def get_melhor_jogada(tabuleiro, jogador1):
-	jogadas = get_jogadas(tabuleiro, jogador1)
+from Tabuleiro.Tabuleiro import *
 
-	melhor_valor = -sys.maxsize - 1  # Menor inteiro
-	melhor_jogada = None
+class IA:
+	jogador_1 = '1'
+	jogador_2 = '2'
 
-	for jogada in jogadas:
-		valor_jogada = minimax(tabuleiro, 3, jogador1)
+	def __init__(self):
+		# Inicializa tabuleiro
+		self.tabuleiro = Tabuleiro(self.jogador_1, self.jogador_2)
 
-		if valor_jogada > melhor_jogada:
-			melhor_valor = valor_jogada
-			melhor_jogada = jogada
+		# Pesos das peças pra usar na heurística
+		self.pesos = {'peao': 10, 'cavalo': 30, 'bispo': 30, 'torre': 50, 'rei': 90, 'rainha': 900, '': 0}
 
-	return melhor_jogada
+	def get_melhor_jogada(self, jogador1):
+		jogadas = self.get_jogadas_possiveis()
 
-def get_jogadas(tabuleiro, jogador1):
-	return [[[1, 0], [-1, 0]],
-			[[0, 1], [0, -1]]]
-
-def minimax(tabuleiro, profundidade, maximizando_player1):
-	# Limite da profundidade atingido
-	if profundidade == 0:  # TODO: Ou jogada é final
-		return avalia_tabuleiro(tabuleiro, maximizando_player1)
-
-	jogadas = get_jogadas()
-	if maximizando_player1:
 		melhor_valor = -sys.maxsize - 1  # Menor inteiro
+		melhor_jogada = None
+
+		for jogada in jogadas:
+			valor_jogada = self.minimax(3, jogador1)
+
+			if valor_jogada > melhor_valor:
+				melhor_valor = valor_jogada
+				melhor_jogada = jogada
+
+		return melhor_jogada
+
+	def get_jogadas_possiveis(self):
+		return [[]]
+
+	def minimax(self, profundidade, maximizando_player1):
+		# Limite da profundidade atingido
+		if profundidade == 0:  # TODO: Ou jogada é final
+			return self.avalia_tabuleiro(maximizando_player1)
+
+		jogadas = self.get_jogadas_possiveis()
+		if maximizando_player1:
+			melhor_valor = -sys.maxsize - 1  # Menor inteiro
+
+			for jogada in jogadas:
+				self.joga_jogada(jogada)
+				melhor_valor = max(melhor_valor, self.minimax(profundidade - 1, not maximizando_player1))
+				self.desfaz_jogada(jogada)
+
+		else:
+			melhor_valor = sys.maxsize  # Maior inteiro
+
+			for jogada in jogadas:
+				self.joga_jogada(jogada)
+				melhor_valor = min(melhor_valor, self.minimax(profundidade - 1, not maximizando_player1))
+				self.desfaz_jogada(jogada)
+
+		return melhor_valor
+
+	def avalia_tabuleiro(self, maximizando_player1):
+		valor_heuristica = self.heuristica_simples()
 		
-		for jogada in jogadas:
-			tabuleiro = joga_jogada(tabuleiro, jogada)
-			melhor_jogada = max(melhor_jogada, minimax(tabuleiro, profundidade - 1, !maximizando_player1))
-	else:
-		melhor_valor = sys.maxsize  # Maior inteiro
+		if maximizando_player1:
+			return valor_heuristica
+		else:
+			return -valor_heuristica
 
-		for jogada in jogadas:
-			tabuleiro = joga_jogada(tabuleiro, jogada)
-			melhor_jogada = min(melhor_jogada, minimax(tabuleiro, profundidade - 1, !maximizando_player1))
+	def heuristica_simples(self):
+		somatorio = 0
+		for i in range(8):
+			for j in range(8):
+				peca = self.tabuleiro.pecas[i][j]
 
-	return melhor_jogada
+				if peca is not None:
+					tipo = peca.tipo()
+					jogador = peca.jogador
 
-def avalia_tabuleiro(tabuleiro, maximizando_player1):
-	valor_heuristica = heuristica(tabuleiro)
-	if maximizando_player1:
-		return valor_heuristica
-	else:
-		return -valor_heuristica
+					if jogador == self.jogador_1:
+						somatorio += self.pesos[tipo]
+					else:
+						somatorio -= self.pesos[tipo]
+		return somatorio
 
-def heuristica(tabuleiro):
-	return random.random() * 10
+	def joga_jogada(self, jogada):
+		self.tabuleiro = self.tabuleiro
 
-def joga_jogada(tabuleiro, jogada):
-	return tabuleiro
+	def desfaz_jogada(self, jogada):
+		self.tabuleiro = self.tabuleiro
