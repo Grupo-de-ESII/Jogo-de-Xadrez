3a2c459b955e26f9bdb981609ebe6cd86caa4d0c
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2018-12-02 01:28:51 -0200

    teste de tabuleiro melhorado

--- src/testeTabuleiro.py
+++ src/testeTabuleiro.py
@@ -7,6 +7,7 @@ d={'branca':1, 'preta':2}
 #XequeMate não explode no começo
 assert tab.xequeMate(d[tab.pecas[0][0].jogador.cor]) == False
 assert tab.xequeMate(d[tab.pecas[0][7].jogador.cor]) == False
+jog=tab.pecas[0][1].jogador
 
 #vendo se deixa algum lixo que torne o proximo xequeMate explodir
 tab.falsoMovimento((0,1),(0,3))
@@ -14,6 +15,9 @@ tab.reset()
 
 assert tab.xequeMate(d[tab.pecas[0][0].jogador.cor]) == False
 assert tab.xequeMate(d[tab.pecas[0][7].jogador.cor]) == False
+assert tab.pecas[0][1] is not None
+assert tab.pecas[0][1].jogador == jog
+
 
 #agora é para valer
 tab.move((0,1),(0,3))
